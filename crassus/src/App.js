585adf970756eb487bb4ed56b5c53778731d9a23
chuber2 <chris.huber386@gmail.com>
2022-06-12 14:52:57 -0400

    dfa

--- crassus/src/App.js
+++ crassus/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { callBackend } from './datapull/helpers'
 
 function App() {
+  const message = callBackend()
+  console.log(message)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello, my name is Crassus! </p>
       </header>
     </div>
   );
